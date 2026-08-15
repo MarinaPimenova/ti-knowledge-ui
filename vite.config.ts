23962d725ffe9e0f436402d66352a033d7d12faa
Maksim Pimenov <mnpimenov@gmail.com>
2026-08-15 05:36:20 +0000

    feature: add CI workflow

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     build: {
         minify: true,
         sourcemap: false,
-        target: 'modules',
+        target: 'es2015',
     },
     css: {
         preprocessorOptions: {
