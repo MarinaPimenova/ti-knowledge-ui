61a6cbf9f90316da16c371985a13e54b6481cd32
MarinaPimenova <mnpma@mail.ru>
2026-08-17 11:13:16 +0300

    TI-15: Observability - Grafana dashboards + Question: CRUD operations (#5)
    Co-authored-by: MarinaPimenova <mnfpma@gmail.com>

--- src/question/question.payload.interface.ts
+++ src/question/question.payload.interface.ts
@@ -1,16 +1,63 @@
-export interface QuestionPayload {
-    question: string;
-    shortAnswer: string;
-    resourceUrl?: string;
-    description?: string;
-}
-
 export interface QuestionRecord {
-    id: string;
+    id: number;
     question: string;
     shortAnswer: string;
     tag: string;
     projectName: string;
-    resourceUrl?: string;
-    description?: string;
+}
+
+export interface TagDto {
+    id: number;
+    tag: string;
+}
+
+export interface ProjectDto {
+    id: number;
+    name: string;
+}
+
+export interface ResourceDto {
+    id?: number;
+    url: string;
+    description: string;
+}
+
+export interface ResourceRequest {
+    url: string;
+    description: string;
+}
+
+export interface CodeExampleDto {
+    language: string;
+    sourceCode: string;
+}
+
+export interface QuestionLevelDto {
+    questionLevelId?: number;
+    difficultyCode?: string;
+}
+
+export interface QuestionDetails {
+    id: number;
+    question: string;
+    shortAnswer: string;
+    detailedAnswer?: string;
+    questionLevel: QuestionLevelDto;
+    codeExample?: CodeExampleDto;
+    tags: TagDto[];
+    resources: ResourceDto[];
+    projects: ProjectDto[];
+    createdBy?: string;
+    updatedBy?: string;
+}
+
+export interface CreateQuestionRequest {
+    question: string;
+    shortAnswer: string;
+    detailedAnswer?: string;
+    questionLevelId?: number;
+    codeExample?: CodeExampleDto;
+    tagIds: number[];
+    projectIds: number[];
+    resources: ResourceRequest[];
 }
